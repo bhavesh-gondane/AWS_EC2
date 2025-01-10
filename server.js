8df0dd3b7024b40d10f3e2b836979120e2b426a5
bhavesh <bhaveshgondane2018@gmail.com>
2025-01-10 20:34:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var express=require('express');
+var session = require('express-session');
+var parseurl = require('parseurl');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var sessionOptions={
+    secret:"secret",
+    resave:true,
+    saveUninitialized:false
+};
+app.use(session(sessionOptions));
+
+app.use(function(req,res,next){
+    if(!req.session.views){
+        req.session.views={};
+        req.session.views.shoppingCart=[];
+    }
+
+    var pathname=parseurl(req).pathname;
+
+    req.session.views[pathname]=(req.session.views[pathname]||0)+1;
+    next();
+})
+
+var staticFolder=express.static(path.join(__dirname,"public"));
+app.use(staticFolder);
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname+"/inddex.html"));
+});
+
+app.listen(8000);
+console.log("Shopping cart web app is listening on port 8000");
